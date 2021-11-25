e5ebe1dac96014a7685afd14b5758d9c4c27c995
Blairoo <87243056+Blairoo@users.noreply.github.com>
2021-11-25 19:07:06 +0900

    fix modal form
    post글 modal에서 form 전송하기
    community.html에서 script 작동하도록 했음
    views.py 문제 - form.is_valid 오류

--- veginner/Post/views.py
+++ veginner/Post/views.py
@@ -2,18 +2,31 @@
 from __future__ import unicode_literals
 from django.shortcuts import redirect, render
 from .models import Vegan_type, Post
+from User.models import User
 from .forms import PostForm
+import sweetify
 
 # Create your views here.
 def community(req):
     print(req.session.session_key)
+    print(req.session.get(req.session.session_key))
     vegan = Post.objects.filter(post_vegan_type=1)
     if req.method == 'POST':
+        nickname=req.POST.get("username")
+        print("community post 들어옴")
         form = PostForm(req.POST)
+        print(form)
         if form.is_valid():
-            form.instance.writer = self.req.user
+#             form.cleaned_data.get("")
+            form.instance.writer = User.objects.get(nickname=nickname)
+
+#             user = authenticate(req, writer)
             form.save()
-        return redirect(req, "community")
+            print("폼저장")
+        else:
+            form.instance.writer = User.objects.get(nickname=nickname)
+            form.save()
+        return redirect("community")
     else:
         form = PostForm()
         context = {
@@ -24,7 +37,10 @@ def community(req):
 #     post = Post.objects.filter(post_vegan_type="Vegan")
 
 def posting(req):
-    #form data save()
-    return redirect(req, "community")
+    form = PostForm()
+#     req.POST.get()
+    form.save()
+    sweetify.info(req, "성공적으로 보내졌습니다.", timer=1200)
+    return redirect("community")
 def about(req):
     return render(req, 'Post/about.html')
