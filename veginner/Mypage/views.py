65197469670e0c39c7d06495ee73bccf5032c1a6
w3dog1 <86637046+w3dog1@users.noreply.github.com>
2021-11-30 19:31:03 +0900

    feat: MyPage monthlyreport
    report highchart로 뿌려 줄 수는 있으나 값에 대한 정확도가 없음

--- veginner/Mypage/views.py
+++ veginner/Mypage/views.py
@@ -1,8 +1,11 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+from django.db.models import Count, Q
 from django.shortcuts import render
 from User.models import User
+from Post.models import Vegan_type, Post
+
 
 # Create your views here.
 def myinfo(req):
@@ -12,4 +15,16 @@ def myposting(req):
     return render(req, 'Mypage/myposting.html')
 
 def monthlyreport(req):
-    return render(req, 'Mypage/monthlyreport.html')
+    print(' chart go chart go !')
+    dataset = Vegan_type.objects \
+    .values('vegan_sort') \
+    .annotate(Vegetarian_count=Count('vegan_sort'),
+     Semi_Vegetarian_count=Count('vegan_sort')).order_by('vegan_sort')
+#     dataset = Post.objects \
+#         .values(User.objects.get()'vegan_type') \
+#         .annotate(Vegan_count=Count('vegan_type'), Lacto_count=Count('vegan_type.Lacto'), Ovo_count=Count('vegan_type.Ovo'), Lacto_Ovo_count=Count('vegan_type.Lacto-Ovo'),Pesco_count=Count('vegan_type.Pesco'),Pollo_count=Count('vegan_type.Pollo'),
+#         Flexitarian_count=Count('vegan_type.Flexitarian')).order_by('vegan_type')
+    print(Vegan_type)
+    print(Vegan_type.objects.values('vegan_type'))
+    print('쿼리셋 생김')
+    return render(req, 'Mypage/monthlyreport.html', {'dataset': dataset})
