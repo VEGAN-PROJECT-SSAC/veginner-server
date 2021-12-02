176a1735709c6dde1930e5c87f7203f84ccbd5c7
Blairoo <87243056+Blairoo@users.noreply.github.com>
2021-12-02 19:06:25 +0900

    feat 게시글 ordering, sorting

--- veginner/Post/urls.py
+++ veginner/Post/urls.py
@@ -4,6 +4,5 @@ from django.urls import path
 urlpatterns = [
     path('about', views.about, name='about'),
     path('community', views.community, name='community'),
-#     path('community/<str:pk>', views.sortpost, name='sort')
     path('posting', views.posting, name='posting'),
 ]
--- veginner/Post/views.py
+++ veginner/Post/views.py
@@ -1,16 +1,18 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 from django.shortcuts import redirect, render
+from django.urls import reverse
 from .models import Vegan_type, Post
 from User.models import User
 from .forms import PostForm
 import sweetify
 import datetime
+from django.contrib.sessions.models import Session
+from django.db.models import Count
 
 # Create your views here.
 def community(req):
-    print(req.session.session_key)
-    print(req.session.get(req.session.session_key))
+#     print(Session.objects.get(pk=req.session.session_key).get_decoded()["_auth_user_id"])# session에서 User pk 가져오기
     v_type = [8, 9, 10, 11]
     sv_type = [12, 13, 14]
     vegan = Post.objects.filter(post_vegan_type__in=v_type)
@@ -31,21 +33,45 @@ def community(req):
             post.content = form.cleaned_data["content"]
             post.save()
             print("폼저장")
+
         else:
             print("유효성 또 실패")
             sweetify.warning(req, form.non_field_errors(), timer=1200)
-        return redirect("community")
+        return redirect('community')
     else:
+        print('GET방식 들어옴')
         form = PostForm()
+        sort = req.GET.get('sort', '')# sorting
+        order = req.GET.get('order', '')# ordering
+        page = req.GET.get('page', '')# pagination
+        post = Post.objects.all().order_by('-write_time')
+        # sorting
+        if sort == 'All':
+            post = Post.objects.all()
+        if sort == 'Vegan':# 8000/community?sort=Vegan
+            post = Post.objects.filter(post_vegan_type__in=v_type)
+        elif sort == 'Semi_Vegan':# choice로 필터링 하는거 노가다방식, 더 좋은 방식이 있는지 모르겠어요 찾아봐야해
+            post = Post.objects.filter(post_vegan_type__in = sv_type)
+
+        # ordering
+        if order == 'Recent':# 8000/community?sort=Vegan&order=Recent
+            # post = Post.objects.annotate(like=Count(''))
+            post = Post.objects.order_by('-write_time')
+        elif order == 'Likes':
+            post = Post.objects.filter(post_vegan_type=8).annotate(like_count=Count('like')).order_by('-like_count')
+#             print(Post.objects.annotate(like_count=Count('like')))
+#             print(Post.objects.get(post_id=1017).like.count())
+#             like_count = Post.objects.order_by('-like_count')
+#             print(like_count)
+        elif order == 'Default':
+            pass
+
+        print(post)
         context = {
-            "vegan": vegan,
-            "semi": semi_vegan,
+            "post": post,
             "form": form
         }
         return render(req, 'Post/community.html', context)
-def sortpost(req, pk):
-    exh = get_objects_or_404(Post, pk=pk)
-    pass
 def posting(req):
     form = PostForm()
 #     req.POST.get()
