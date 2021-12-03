deccb8f908b229149952f7b9e461ff3ad6957ef0
Blairoo <87243056+Blairoo@users.noreply.github.com>
2021-12-03 19:10:18 +0900

    feat&fix 정렬기능 구현
    정렬 기능 구현, annotate issue 해결

--- veginner/Post/views.py
+++ veginner/Post/views.py
@@ -12,12 +12,6 @@ from django.db.models import Count
 
 # Create your views here.
 def community(req):
-#     print(Session.objects.get(pk=req.session.session_key).get_decoded()["_auth_user_id"])# session에서 User pk 가져오기
-    v_type = [8, 9, 10, 11]
-    sv_type = [12, 13, 14]
-    vegan = Post.objects.filter(post_vegan_type__in=v_type)
-    semi_vegan = Post.objects.filter(post_vegan_type__in=sv_type)
-
     if req.method == 'POST':
 #         nickname=req.POST.get("username")
         print("community post 들어옴")
@@ -39,47 +33,63 @@ def community(req):
             sweetify.warning(req, form.non_field_errors(), timer=1200)
         return redirect('community')
     else:
-        print('GET방식 들어옴')
         form = PostForm()
         sort = req.GET.get('sort', '')# sorting
         order = req.GET.get('order', '')# ordering
         page = req.GET.get('page', '')# pagination
-        post = Post.objects.all().order_by('-write_time')
-        # sorting
-        if sort == 'All':
-            post = Post.objects.all()
-        if sort == 'Vegan':# 8000/community?sort=Vegan
-            post = Post.objects.filter(post_vegan_type__in=v_type)
-        elif sort == 'Semi_Vegan':# choice로 필터링 하는거 노가다방식, 더 좋은 방식이 있는지 모르겠어요 찾아봐야해
-            post = Post.objects.filter(post_vegan_type__in = sv_type)
-
+        post_value = Post.objects.values('post_id', 'writer__nickname', 'food_name', 'post_vegan_type__vegan_type', 'date', 'image', 'content')
+        posts = post_value.order_by('-write_time')# default posts
+        vegan_type_dict = dict()
+        for obj in Vegan_type.objects.all():
+            vegan_type_dict[obj.vegan_type] = obj.vegan_id
+#         order_by_dict = {'Recent': '-write_time', 'Likes': '-like_count', '-write_time'}
+        if sort != '' and order != '':
+            if order == 'Likes':
+                posts = post_value.annotate(like_count=Count('like')).order_by('-like_count','-write_time')
+            else:
+                posts = post_value.filter(post_vegan_type=vegan_type_dict[sort]).order_by('-write_time')
+        elif sort != '' and order == '':
+            posts = post_value.filter(post_vegan_type=vegan_type_dict[sort]).order_by('-write_time')
+        elif sort == '' and order != '':
+            if order == 'Likes':
+                posts = post_value.annotate(like_count=Count('like')).order_by('-like_count','-write_time')
+            else:
+                posts = post_value.order_by('-write_time')
         # ordering
-        if order == 'Recent':# 8000/community?sort=Vegan&order=Recent
-            # post = Post.objects.annotate(like=Count(''))
-            post = Post.objects.order_by('-write_time')
-        elif order == 'Likes':
-            post = Post.objects.filter(post_vegan_type=8).annotate(like_count=Count('like')).order_by('-like_count')
+#         if order == 'Recent':# 8000/community?sort=Vegan&order=Recent
+#             # post = Post.objects.annotate(like=Count(''))
+#             post = Post.objects.order_by('-write_time')
+#         elif order == 'Likes':
+# #             post = Post.objects.values_list('post_vegan_type').annotate(like_count=Count('like')).order_by('-like_count','-write_time')
+#             post = Post.objects.values('post_id', 'writer__nickname', 'food_name', 'post_vegan_type__vegan_type', 'date', 'image', 'content').annotate(like_count=Count('like')).order_by('-like_count','-write_time')
+#             post = Post.objects.annotate(like_count=Count('like')).order_by('-like_count','-write_time').distinct('post_id')
+#             post = Post.objects.order_by('-like','-write_time').values('writer', 'food_name').distinct()
+#             print(Post.objects.values('like_count'))
+#             post = Post.objects.annotate(like_count=Count('like')).order_by('-like_count','-write_time')[0]
+#             post = Post.objects.order_by('-like')
+#             for l in likes:
+#                 print(l)
 #             print(Post.objects.annotate(like_count=Count('like')))
 #             print(Post.objects.get(post_id=1017).like.count())
 #             like_count = Post.objects.order_by('-like_count')
 #             print(like_count)
-        elif order == 'Default':
-            pass
-
-        print(post)
+#             posting = Post.objects.prefetch_related('User_set').annotate(like_count=Count('like')).order_by('-like_count','-write_time')
+#             print(posting)
+#             print(Post.objects.all().order_by('like').distinct('post_id'))
+        print(posts)
         context = {
-            "post": post,
+            "post": posts,
             "form": form
         }
         return render(req, 'Post/community.html', context)
 def posting(req):
     form = PostForm()
-#     req.POST.get()
     form.save()
     sweetify.info(req, "성공적으로 보내졌습니다.", timer=1200)
-
-
-
     return redirect("community")
+
 def about(req):
     return render(req, 'Post/about.html')
+
+def detail(req):
+    return render(req, 'Post/detail.html')
