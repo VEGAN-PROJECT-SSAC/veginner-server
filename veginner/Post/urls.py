a86d81ea5b55e721a2283d7197be51837544567a
kodayoung <kodayoung1@gmail.com>
2021-12-03 19:24:43 +0900

    feat.디테일,내가쓴글
    detail페이지, 내가쓴글보기 views&html

--- veginner/Post/urls.py
+++ veginner/Post/urls.py
@@ -4,5 +4,6 @@ from django.urls import path
 urlpatterns = [
     path('about', views.about, name='about'),
     path('community', views.community, name='community'),
+    path('community/detail/', views.detail, name='detail'),
     path('posting', views.posting, name='posting'),
 ]
